1f10a089c6b2a1a3dfe326e63c263f1cd598950e
MDz1985 <m_d@tut.by>
2021-08-29 09:07:17 +0300

    feat: play button works

--- custom-video-player/script.js
+++ custom-video-player/script.js
@@ -1,2 +1,18 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.video');
+const progressSlider = player.querySelector('progressSlider');
+const playButton = player.querySelector('.playButton');
+const playCentral = player.querySelector('.central');
+
+
+
+
+//PLAY
+function play() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+    if (video.paused){playCentral.style.zIndex = '10';}
+   else {playCentral.style.zIndex = '0';}
+}
+playButton.addEventListener('click', play);
+playCentral.addEventListener('click', play)
